e95a1eef424cab601e5cfe9432e8b3d46fd28d03
PC- <knyshov2002@mail.ru>
2023-01-10 19:03:59 +0500

    принты

--- Bubble.cs
+++ Bubble.cs
@@ -6,11 +6,17 @@ public class Bubble
         {
            for (int i = 0; i < mas.Length-1; i++)
            {
+               Console.WriteLine($"Сравниваем элементы {mas[i]} и {mas[i+1]}");
                    if (mas[i] > mas[i+1])
                    {
+                       Console.WriteLine($"{mas[i]}>{mas[i+1]} поэтому меняем их местами");
                        (mas[i], mas[i+1]) = (mas[i+1], mas[i]);
                    }
+
+                   Console.WriteLine($"Элемент {mas[i]}<{mas[i+1]} поэтому не меняем их местами");
            }
+
+           Console.WriteLine("Массив отсортирован");
            return mas;
        }
    
